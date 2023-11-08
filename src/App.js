253bcacbe69140e8d1198f9d48b2bcf878b9eef2
JayDeep-Giri <jaydeepgiri30@gmail.com>
2023-11-08 11:12:05 +0530

    Updated React-website

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React from 'react';
+import TopBar from './components/TopBar';
+import Navbar from './components/Navbar';
+import Hero from './components/hero';
+import Activities from './components/Activities';
+import Booking from './components/Booking';
+import Gallery from './components/Gallery';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div>
+      <TopBar />
+      <Navbar />
+      <Hero />
+      <Activities />
+      <Booking />
+      <Gallery />
+      <Contact />
+      <Footer />
     </div>
   );
 }
