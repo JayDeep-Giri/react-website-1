8709159219283809e6ec3824683ee7899fde157b
JayDeep-Giri <jaydeepgiri30@gmail.com>
2023-11-08 10:13:43 +0530

    Added Activities Section

---
+++ src/components/Activities.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Activities = () => {
+  return (
+    <div className='max-w-[1140px] m-auto w-full md:flex mt-[-75px]'>
+        <div className='relative p-4'>
+            <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Resorts</h3>
+            <img
+                className='w-full h-full object-cover relative border-4 border-white shadow-lg'
+                src='https://images.unsplash.com/photo-1696813051273-bd811463ecb4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE0M3xobWVudlFoVW14TXx8ZW58MHx8fHx8' alt='/'/>
+        </div>
+        <div className='relative p-4'>
+            <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Resorts</h3>
+            <img
+                className='w-full h-full object-cover relative border-4 border-white shadow-lg'
+                src='https://images.unsplash.com/photo-1695394979617-f43bbf569deb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE0OXxobWVudlFoVW14TXx8ZW58MHx8fHx8' alt='/'/>
+        </div>
+        <div className='relative p-4'>
+            <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Resorts</h3>
+            <img
+                className='w-full h-full object-cover relative border-4 border-white shadow-lg'
+                src='https://plus.unsplash.com/premium_photo-1692689475641-fae2eb24c145?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDE1MHxobWVudlFoVW14TXx8ZW58MHx8fHx8' alt='/'/>
+        </div>
+    </div>
+  )
+}
+
+export default Activities
